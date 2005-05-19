1cd7f2ebc1259bbc6c9193d9d182f3759065ae8a
Stefano Pintore <stefano.pintore@ingv.it>
2005-05-19 08:04:02 +0000

    Gimp plug-in Teseo-2 per GIMP-2 	- Testato configure, make, make install 	- Interfaccia grafica costruita con glade-2
    	TODO
    	- pixmaps non attivi
    	- po files da sistemare
    	- interface.c da verificare
    	- riempire i callbacks
    
    
    git-svn-id: svn+ssh://svn.rm.ingv.it/svn/teseo2/trunk@10 afdc3c29-bc1c-424d-8b75-1d331506fbbc

---
+++ teseo-dist/teseo-2/src/teseocallbacks.h
@@ -0,0 +1,130 @@
+#include <gtk/gtk.h>
+
+
+void
+on_new1_activate                       (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_open1_activate                      (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_save1_activate                      (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_save_as1_activate                   (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_preferences_2_activate              (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_path2_activate                      (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_dxf2_activate                       (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_trace2_activate                     (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_sac2_activate                       (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_sac_xy2_activate                    (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_sisma2_activate                     (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_timemark2_activate                  (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_path1_activate                      (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_dxf1_activate                       (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_track1_activate                     (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_sac1_activate                       (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_sac_xy1_activate                    (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_sisma1_activate                     (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_timemark1_activate                  (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_quit1_activate                      (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_resample1_activate                  (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_align_all1_activate                 (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_link_all1_activate                  (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_move_and_rotation1_activate         (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_fitting_polyline1_activate          (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_fitting_bezier1_activate            (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_recover_last2_activate              (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_evaluate_middle_tms1_activate       (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_adjustment1_activate                (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_info_window_1_activate              (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_help1_activate                      (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_about1_activate                     (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
