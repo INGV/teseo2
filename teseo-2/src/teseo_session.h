a3054fa4a90be604786e961ca66070e526b3e7c1
Stefano Pintore <stefano.pintore@ingv.it>
2005-07-11 09:33:30 +0000

    Iniziato lavoro su sessione.
    git-svn-id: svn+ssh://svn.rm.ingv.it/svn/teseo2/trunk@93 afdc3c29-bc1c-424d-8b75-1d331506fbbc

---
+++ teseo-2/src/teseo_session.h
@@ -0,0 +1,122 @@
+/* Teseo-2 Plug-in
+ * Copyright (C) 2005  Stefano Pintore, Matteo Quintiliani (the "Authors").
+ * All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE Authors BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Except as contained in this notice, the name of the Authors of the
+ * Software shall not be used in advertising or otherwise to promote the
+ * sale, use or other dealings in this Software without prior written
+ * authorization from the Authors.
+ */
+
+/*
+
+The vectorising session includes:
+
+Event info:
+	Date: Event Date
+	Channel code: 2 char
+	Component code
+	Station code
+	Station location: lat, long, quote
+
+Image info:
+	XCFfilename
+	Resolution,....
+
+Traces info:
+
+	SVG filename
+	Trace type: event, time-marker
+	Sampling rate: sample/mm
+	First sample Time
+
+Vectorising Preferences: vectorizing settings
+
+Notes: User notes
+
+*/
+
+#ifndef TESEOSESSION
+#define TESEOSESSION 1
+
+#include "teseo_io.h"
+
+struct teseoDate{
+	long year;
+	int month;
+	int day;
+};
+
+struct teseoTime{
+	int hours;
+	int minutes;
+	int seconds;
+};
+
+struct Location{
+	double lat;
+	double lon;
+	double quote;
+};
+
+struct EventInfo{
+	struct teseoDate EventDate;
+	char ChCode[2];
+	char CompCod;
+	char StaCode[5];
+	struct Location StaLocation;
+};
+
+struct ImageInfo{
+	char XCFfilename[FILENAMELEN];
+	long Res;
+};
+
+enum TraceType{EVENT,TIMEMARKER};
+
+struct TracesInfo{
+	char SVGFilename[FILENAMELEN];
+	char tracetype;
+	long SamplingRate;
+	struct teseoTime FirstSampleTime;
+};
+
+
+struct Preferences{
+ char dummy;
+};
+
+struct Notes{
+	char  text[255];
+};
+
+struct Session{
+	struct EventInfo   eventinfo;
+	struct ImageInfo   imageinfo;
+	struct TracesInfo  tracesinfo;
+	struct Preferences preferences;
+	struct Notes       notes;
+};
+
+
+char save_session(char * filename);
+char load_session(char * filename);
+
+#endif
