029fe5b0198c19cd7a0c173f1b4b850e6ea4e07c
Alberto Michelini <alberto.michelini@ingv.it>
2005-08-25 12:11:00 +0000

    c created the program driver1 to 'drive' the routine  1_arm-shift-value-en_sub.f c and modified opportunely the rotuine itself.
    git-svn-id: svn+ssh://svn.rm.ingv.it/svn/teseo2/trunk@277 afdc3c29-bc1c-424d-8b75-1d331506fbbc

---
+++ utils-no-dist/third-party/schlupp/driver1.f
@@ -0,0 +1,55 @@
+      program driver1
+c
+c this driver program calls the 1_arm-shift-value-en_sub routine written
+c by Antoine Schlupp
+c
+
+	character*20 fiche
+	character*20 sortie
+c	character*20 sortie2
+
+c	integer i,cpt
+
+	real yi,yf
+	real sec,alp,ret
+c	real x(15000),y(15000),xx(15000),yy(15000)
+c	real xa(15000),ya(15000)
+	real xinitial,xfinal
+	real Bg,r,Rg,a,b,xtest
+
+ 	write(*,*)'Name of input file'
+	read(*,*) fiche
+ 	write(*,*)'Name of output file'
+	read(*,*) sortie
+ 	write(*,*)'enter the two extremes points of the input file, * xinitial xfinal yi yf, along the base line:'
+	read(*,*) xinitial,xfinal,yi,yf
+ 	write(*,*)'number of seconds between  xinitial and xfinal:'
+	read(*,*) sec  	
+ 
+c	Correction of the curvature of the signal due to the geometry  
+c	of the Wiechert seismograph recorder
+ 
+	write(*,*) 'Enter the velocity of the paper in mm/min :'
+	read(*,*)  Bg
+	write(*,*) 'Enter the radius of the cylinder that carry the paper, in mm:'
+	read(*,*)  r
+	write(*,*) 'Enter the length of the writting arm from its rotation axe 
+     *until the end of the needle in mm :'
+	read(*,*)  Rg	
+	write(*,*) 'Enter the distance from the rotation axe of the writing arm 
+     *until the axe of the cylinder that carry the paper 
+     *measured perpendicular to the cylinder axe in mm :'
+	read(*,*)  a
+	write(*,*) 'Enter the shift between the needle position at rest and its 
+     *normal equilibrium position (arm perpendicular to the axe of the cylinder)
+     * value positive if the arm is inclined downward  
+     *  in mm :'
+	read(*,*)  b
+ 
+c
+      call 1_arm-shift-value-en_sub(fiche, sortie, xinitial,
+     +         xfinal,yi,yf,sec, Bg,r, Rg, a, b, ret)
+c
+      stop
+      end
+
