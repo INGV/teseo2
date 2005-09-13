eb3e57eccb07e516799a1a1c2846416a5f5e45ab
Matteo Quintiliani <matteo.quintiliani@ingv.it>
2005-09-13 12:46:31 +0000

    Added evaluate middle timemark function
    git-svn-id: svn+ssh://svn.rm.ingv.it/svn/teseo2/trunk@392 afdc3c29-bc1c-424d-8b75-1d331506fbbc

---
+++ teseo-2/src/teseo_timemark.h
@@ -0,0 +1,40 @@
+/* Teseo-2 Plug-in
+ * Copyright (C) 2005  Stefano Pintore, Matteo Quintiliani (the "Authors").
+ * All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE Authors BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Except as contained in this notice, the name of the Authors of the
+ * Software shall not be used in advertising or otherwise to promote the
+ * sale, use or other dealings in this Software without prior written
+ * authorization from the Authors.
+ */
+ 
+#ifndef __TESEO_TIMEMARK_H__
+#define __TESEO_TIMEMARK_H__ 1
+
+
+
+/*!
+  teseo_timemark_test_path returns abscissas list or NULL. Function ckeck in the current timemark path some  missing points.
+  \param gint32 teseo_image ID of the GIMP image
+  */
+GList* teseo_timemark_check_path(gint32 teseo_image);
+
+
+#endif
