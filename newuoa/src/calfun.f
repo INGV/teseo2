4045762eea927697e8745ab01a5dc6df3e3eb81b
Stefano Pintore <stefano.pintore@ingv.it>
2005-10-03 13:18:57 +0000

    Modified name of the function called.
    git-svn-id: svn+ssh://svn.rm.ingv.it/svn/teseo2/trunk@471 afdc3c29-bc1c-424d-8b75-1d331506fbbc

--- newuoa/src/calfun.f
+++ newuoa/src/calfun.f
@@ -1,8 +1,9 @@
       SUBROUTINE CALFUN (N,X,F)
-c      F=0.0D0
+c
       REAL*8 X(4)
       REAL*8 F
-      F = TESEO_P_FUNC_SUM_DISTANCE_BEZIER(X)
+c     calling the c function myfun
+      F=CMYFUN(X)
       RETURN
       END
 
