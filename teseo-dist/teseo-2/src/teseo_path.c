8d3be14da369fe5d271aec4ba0b14631d1cb7407
Matteo Quintiliani <matteo.quintiliani@ingv.it>
2009-02-23 11:53:32 +0000

    Added g_printf warning for unevelny spaced path
    git-svn-id: svn+ssh://svn.rm.ingv.it/svn/teseo2/trunk@816 afdc3c29-bc1c-424d-8b75-1d331506fbbc

--- teseo-dist/teseo-2/src/teseo_path.c
+++ teseo-dist/teseo-2/src/teseo_path.c
@@ -123,6 +123,7 @@ path_semantic_type teseo_path_semantic_type_even(gint32 g_image, gchar *path_nam
 		    *delta_pix = points_pairs[9] - points_pairs[3];
 		    sum_delta_pix += *delta_pix;
 		    n_delta_pix++;
+		    g_printf("First delta_pix %f\n", *delta_pix);
 		    if(*delta_pix < 0.0) {
 			    *delta_pix = 0.0;
 		    }
@@ -154,6 +155,7 @@ path_semantic_type teseo_path_semantic_type_even(gint32 g_image, gchar *path_nam
 		    cur_delta_pix = (points_pairs[i+3] - points_pairs[i-6]);
 		    if( fabs ( cur_delta_pix - *delta_pix ) > precision ) {
 			    *delta_pix = 0.0;
+			    g_printf("Path is not evenly spaced between %f %f\n", points_pairs[i-6], points_pairs[i+3]);
 		    } else {
 			sum_delta_pix += cur_delta_pix;
 			n_delta_pix++;
